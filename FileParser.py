b670191daa5ed0dc38987526dee2a5455d54762f
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-02-22 20:25:03 -0600

    Update FileParser.py

--- FileParser.py
+++ FileParser.py
@@ -1,15 +1,5 @@
 from typing import Any, Union
-###import pandas
-#import nltk
-#import re
-#from pandas import DataFrame
-#from pandas.io.parsers import TextFileReader
 
-#file = str(pandas.read_csv('PracticeFile.txt'))
-#print(file)
-#words = {}
-#words = nltk.word_tokenize(file)
-#print(words)
 import io
 
 from nltk.corpus import stopwords
