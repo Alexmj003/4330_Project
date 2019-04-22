dffc9391d56f571a35556ff392a5c92743f8ff56
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-04-21 23:03:09 -0500

    Update FileParser.py

--- FileParser.py
+++ FileParser.py
@@ -1,40 +1,59 @@
 
+
 import collections
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import docx
 
-fo = open("PracticeFile.txt", "r") #open file
-content = fo.read() #readfile
-contentSplit = content.split(' ') #splitString
-stop_words = set(stopwords.words('english'))#stop word filtering
-appendFile = open('filteredtext.txt', 'a')  # new file of filtered words
-appendFile.truncate(0)#Clears Temp file of txt
-appendFile.close() #closes The File
-for r in contentSplit:#loop
-    if not r in stop_words:#filter stop words
-        appendFile = open('filteredtext.txt', 'a') # opens the file again
-        appendFile.write(" "+r) # append to file
-        appendFile.close() # close file
+class FileParser:
+    def __init__(self,):
+        input_file = None
+        content = None# initial text of file
+        content_split = None
+        stop_words = set(stopwords.words('english'))#stop word filtering
+        word_limiter = 10
+        result_file = None
+        filter_words = []
+        keywords = {}
+        wordcounter = None
+        topkeywords = []
 
-FilterFile = open("filteredtext.txt","r") #split
-FilteredContent = FilterFile.read() #filteredcontent
-FilteredContentSplit = FilteredContent.split(' ') #split content
+    def readtxt(self,txt = str("PracticeFile.txt")):
+        self.input_file = open(str,'a')
+        self.contents = self.input_file.read()
+        self.content_split = self.content.split(' ')#splitstring
+        for r in self.content_split:
+            if not r in stop_words:
+                self.filter_words.append(r)
+        for word in self.filter_words:
+            if word not in self.keywords:
+                self.keywords[word] = 1
+            else:
+                self.keywords[word] += 1
+        self.wordcounter = collections.Counter(self.keywords)
+        for word, count in self.wordcounter.most_common(self.word_limiter):
+            self.topkeywords.append(word)
+        self.input_file.close()
 
-wordcount = {} # list of key words
-for word in FilteredContentSplit: #loop for gathering key words
-    if word not in wordcount: #nested loop
-        wordcount[word] = 1 #algoritm
-    else:
-        wordcount[word] += 1 #algorithm
+    def readdocx(self,txt = str('/Users/idky/PycharmProjects/BasicProject/untitled/Corn, Cows, and Cash_word FINAL DRAFT.docx"')):
+        self.input_file = docx.Document(txt)
+        self.content = []
+        for para in self.input_file.paragraphs:
+            self.content.append(para.text)
+        text = "".join(self.content)
+        self.content_split = text.split(' ')
+        for r in self.content_split:
+            if not r in stop_words:
+                self.filter_words.append(r)
+        for word in self.filter_words:
+            if word not in self.keywords:
+                self.keywords[word] = 1
+            else:
+                self.keywords[word] += 1
+        self.wordcounter = collections.Counter(self.keywords)
+        for word, count in self.wordcounter.most_common(self.word_limiter):
+            self.topkeywords.append(word)
 
-
-WordLimiter = 10 #limit for keywords
-word_counter = collections.Counter(wordcount) #collections class for counting words
-FinalFile = open("FinalFile.txt","w") #Opens file to be passed to the wordcloud
-
-for word, count in word_counter.most_common(WordLimiter): # writes words to file
-    FinalFile.write(word + " ")
-
-FilterFile.close()#cleanup
-fo.close()#cleanup
-FinalFile.close()#cleanup
+    def gettopkeywords(self):
+        return self.gettopkeywords()
+    
