45848419ed7c01558d1ecab7c795437b973dde48
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-04-22 19:02:44 -0500

    Update queryall.py

--- queryall.py
+++ queryall.py
@@ -2,17 +2,18 @@ from mysql.connector import MySQLConnection, Error
 from database_config import read_db_config
  
  
-def query_fetchall():
+def query_fetchall(output = str('output.txt')):
     try:
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM documents")
         rows = cursor.fetchall()
- 
-        print('Total Row(s):', cursor.rowcount)
+        file = open(output,'w')
+        file.write('Total Row(s): ' + cursor.rowcount + '\n')
         for row in rows:
-            print(row)
+            file.write(row + '\n')
+        file.close()
  
     except Error as e:
         print(e)
